027f954d9ccf3b4318f76d7ca2c7e8d223dc1d6d
Junyaozero <106330900+Junyaozero@users.noreply.github.com>
2023-06-08 18:56:10 +0800

    添加找回密码页面，完善注册功能及按钮失效功能，实现注册成功后跳转登陆页面
    添加找回密码页面，完善注册功能及按钮失效功能，实现注册成功后跳转登陆页面

--- DynamicBus云开发/miniprogram/pages/loginin/loginin.js
+++ DynamicBus云开发/miniprogram/pages/loginin/loginin.js
@@ -1,64 +1,30 @@
-//调用获取默认环境的数据库的引用
-const db = wx.cloud.database()
-// miniprogram/components/cloudTipModal/index.js
-const { isMac } = require('../../envList.js');
-
-Component({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    disabled:true,//丧失能力
-    btnstate:"default",
-    account:"",
-    password:"",
-    showUploadTip: false,
-    tipText: isMac ? 'sh ./uploadCloudFunction.sh' : './uploadCloudFunction.bat'
-  },
-  properties: {
-    showUploadTipProps: Boolean
-  },
-  observers: {
-    showUploadTipProps: function(showUploadTipProps) {
-      this.setData({
-        showUploadTip: showUploadTipProps
-      });
-    }
-  },
-  methods: {
-    onChangeShowUploadTip() {
-      this.setData({
-        showUploadTip: !this.data.showUploadTip
-      });
+Page({
+    data:{
+        disabled:true,//丧失能力
+        btnstate:"default",
+        account:"",
+        userName:"",
+        password:""
     },
-
-    copyShell() {
-      wx.setClipboardData({
-        data: this.data.tipText,
-      });
-    },
-    
     //获取文本问题
     accountInput:function(e){
         var content = e.detail.value;
         console.log(content);
         if(content!=""){
-            this.setData({disabled:false,btnstate:"primary",account:content});
-        }else{
-            this.setData({disabled:true,btnstate:"default"})
+            this.setData({password:password});
+            // this.setData({disabled:false,btnstate:"primary",account:content});
         }
+            // this.setData({disabled:true,btnstate:"default"})
+        
     },
-
     pwdBlur:function(e){
         var password = e.detail.value;
         if(password != ""){
             this.setData({password:password});
         }
-    },    
-       
-    //跳转
-    navigate(){
+    },
+    //跳转注册页面
+    toRegister(){
         wx.navigateTo({
           url: '/pages/mobile/mobile',
           success(res){
@@ -68,35 +34,20 @@ Component({
           }
         })
       },
+    //跳转找回密码页面
+    toFindPwd(){
+        wx.navigateTo({
+          url: '/pages/findPwd/findPwd',
+          success(res){
+            console.log(res)
+          },fail(err){
+            console.error(err)
+          }
+        })
+      },
+      //提交表单，判断是否登录
+      btnSubmit(){
 
-//       //提交表单添加进数据库
-//   btnSub(res){
-//     console.log(res)
-//     // 老方法取数据
+      }
 
-//     // var userName = res.detail.value.userName;
-//     // var userPwd= res.detail.value.userPwd;
-//     // console.log(userName,userPwd)
-
-//     //解构方法取数据
-
-//     var {userName,userPwd}=res.detail.value;
-//     //console.log(userName,userPwd)
-//     // 直接使用对象传递
-//     var resVlu = res.detail.value
-//     db.collection("user_info").add({
-//         // data:{
-//         //     userName:userName,
-//         //     userPwd:userPwd
-//         // }
-//         data:resVlu
-
-//     }).then(res=>{
-//         console.log(res)
-//     })
-
-//   }      
-      
-  }
-
-});
+})
