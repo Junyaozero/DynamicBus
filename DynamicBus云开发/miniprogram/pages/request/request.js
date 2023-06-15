06270ce48c69982ae5027d368e9bbb294aa29cb5
SunYifei-484 <stu2000484@cgt.bitzh.edu.cn>
2023-06-15 19:02:57 +0800

    提交的需求不能与上一次相同

--- DynamicBus云开发/miniprogram/pages/request/request.js
+++ DynamicBus云开发/miniprogram/pages/request/request.js
@@ -1,4 +1,6 @@
 let submitCount = 0;
+let lastSubmitTime = {};
+
 Page({
   // 页面的初始数据
   data: {
@@ -7,38 +9,37 @@ Page({
     SelectTwo: false,
     dropoffPoint: '请选择'
   },
- 
-    // 点击事件
-     //取反，类似开关操作，点击显示下拉列表
-     bindShowMsg() {
-        this.setData({
-            select:!this.data.select
-        })
-    },
-      bindShowMsgTwo() {
-        this.setData({
-          SelectTwo:!this.data.SelectTwo
-        })
-    },
-      
-      // 选择上车、下车地点并赋值到这个变量到boardingPoint、dropoffPoint，
-     //  选择结束，隐藏下拉列表
-    mySelect(e) {
-       var name = e.currentTarget.dataset.name
-       this.setData({
-          boardingPoint: name,
-           select: false
-       })
-       
-    },
-    mySelectTwo(e) {
-       var name = e.currentTarget.dataset.name
-       this.setData({
-          dropoffPoint: name,
-            SelectTwo: false
-       })
-    },
-  
+
+  // 点击事件
+  //取反，类似开关操作，点击显示下拉列表
+  bindShowMsg() {
+    this.setData({
+      select: !this.data.select
+    })
+  },
+  bindShowMsgTwo() {
+    this.setData({
+      SelectTwo: !this.data.SelectTwo
+    })
+  },
+
+  // 选择上车、下车地点并赋值到这个变量到boardingPoint、dropoffPoint，
+  // 选择结束，隐藏下拉列表
+  mySelect(e) {
+    var name = e.currentTarget.dataset.name
+    this.setData({
+      boardingPoint: name,
+      select: false
+    })
+
+  },
+  mySelectTwo(e) {
+    var name = e.currentTarget.dataset.name
+    this.setData({
+      dropoffPoint: name,
+      SelectTwo: false
+    })
+  },
 
   request() {
     // 获取登录状态
@@ -56,34 +57,52 @@ Page({
         return
       }
 
-
       // 判断上下车地点是否有存在空
       else if (that.data.boardingPoint !== '请选择' && that.data.dropoffPoint !== '请选择') {
 
-         //5分钟内，点击次数不能超过3次
+        //5分钟内，点击次数不能超过3次
         if (submitCount < 3) {
-          submitCount++;
-          setTimeout(() => {
-            submitCount = 0;
-          }, 5 * 60 * 1000); // 5分钟，单位为毫秒
+            submitCount++;
+            setTimeout(() => {
+              submitCount = 0;
+            }, 5 * 60 * 1000); // 5分钟，单位为毫秒
 
-          //跳转到非tabBar页面，并传参
-          wx.navigateTo({
-            url: '../Viewing/Viewing?boardingPoint=' + that.data.boardingPoint + '&dropoffPoint=' + that.data.dropoffPoint,
-            success(res) {
-              console.log(res)
-            }, fail(err) {
-              console.error(err)
-            }
-          });
+          // 本次提交的上车地点和下车地点与上一次提交的不能相同
+          if (that.data.boardingPoint === lastSubmitTime.boardingPoint && that.data.dropoffPoint === lastSubmitTime.dropoffPoint) {
+            wx.showToast({
+              title: '重复提交，请稍后',
+              icon: 'none'
+            });
+            return;
+          }
+    
+          // 更新上一次提交的上车地点和下车地点
+          lastSubmitTime = {
+            boardingPoint: that.data.boardingPoint,
+            dropoffPoint: that.data.dropoffPoint
+          };
 
-        } else {
-          wx.showToast({
-            title: '提交频繁，请稍后',
-            icon: 'none'
-          });
-        }
+
+          
+        // 跳转到非tabBar页面，并传参
+        wx.navigateTo({
+          url: '../Viewing/Viewing?boardingPoint=' + that.data.boardingPoint + '&dropoffPoint=' + that.data.dropoffPoint,
+          success(res) {
+            console.log(res)
+          }, fail(err) {
+            console.error(err)
+          }
+        });
+
+
       } else {
+        wx.showToast({
+          title: '提交频繁，请稍后',
+          icon: 'none'
+        });
+      }
+
+    } else {
         wx.showToast({
           title: '请重新选择',
           icon: 'error',
@@ -105,7 +124,9 @@ Page({
       }, 2000);
     }
   }
-});
+
+})
+
 
 
 
