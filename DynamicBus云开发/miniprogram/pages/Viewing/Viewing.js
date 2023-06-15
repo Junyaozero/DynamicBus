3dd8dd5821612a8be43a2c25bd2a75be353a35ca
SunYifei-484 <stu2000484@cgt.bitzh.edu.cn>
2023-06-15 16:14:36 +0800

    用户5分钟内最多提交3次，查看页面上车下车地点实时更新，上车地点等待人数随着用户的提交而改变，数据库中存入用户选择信息

--- DynamicBus云开发/miniprogram/pages/Viewing/Viewing.js
+++ DynamicBus云开发/miniprogram/pages/Viewing/Viewing.js
@@ -1,32 +1,145 @@
-// pages/Viewing/Viewing.js
-//调用获取默认环境的数据库的引用
 const db = wx.cloud.database()
 
 Page({
-    async onLoad(option) {
-        console.log("传递参数为", option)
-        let {boardingPoint,dropoffPoint} = option
+    data:{
+      userName : ""
+    },
+  async onLoad(option) {
+    console.log("传递参数为", option)
+    let { boardingPoint, dropoffPoint } = option
+    this.setData({
+      boardingPoint,
+      dropoffPoint,
+    })
+
+    //获取本地缓存中‘user’存入userName
+    var user=wx.getStorageSync('user')
+    var user1 = user[0]
+    this.setData({
+        userName:user1.userName
+    })     
+
+    db.collection('request_info').where({
+        userName: this.data.userName
+    }).get().then(res =>{
+        console.log(res)
+        if (res.data.length > 0) {
+            // 用户名存在，更新请求
+            db.collection('request_info').where({
+                userName: this.data.userName
+            }).update({
+                data: {
+                  boardingPoint:boardingPoint,
+                  dropoffPoint:dropoffPoint,
+                  requestTime: new Date()
+                }
+            })
+              } else {
+                // 用户名不存在，新建请求
+                db.collection("request_info").add({
+                    data: {
+                      userName: this.data.userName,
+                      boardingPoint:boardingPoint,
+                      dropoffPoint:dropoffPoint,
+                      requestTime: new Date()
+                    }
+                  }).then(res => {
+                    console.log(res)
+                  }).catch(err => {
+                    console.error(err)
+                  })
+              }
+    })
+    this.count()
+   
+  },
+
+  async count(){
+       // 查询各个上车地点的数量
+    const countResult1 =  await db.collection('request_info').where({
+        boardingPoint: '明德楼'
+      }).count()
+      this.setData({
+        count1: countResult1.total
+      })
+  
+      const countResult2 = await db.collection('request_info').where({
+          boardingPoint: '弘毅楼'
+        }).count()
         this.setData({
-            boardingPoint,
-            dropoffPoint
+          count2: countResult2.total
         })
-        // 获取requestID
-        const countResult = await db.collection('request_info').count()
-        // 时间
-        let date = new Date(new Date().getTime());
-        let Time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-
-        db.collection("request_info").add({
-            data:{
-                boardingPoint,
-                dropoffPoint,
-                requestID:countResult.total,
-                requsetTime: Time
-            }
-           
-        }).then(res=>{
-            console.log(res)
-
+      
+        const countResult3 = await db.collection('request_info').where({
+          boardingPoint: '天佑楼'
+        }).count()
+        this.setData({
+          count3: countResult3.total
         })
-    }
+      
+        const countResult4 = await db.collection('request_info').where({
+          boardingPoint: '艺悦楼'
+        }).count()
+        this.setData({
+          count4: countResult4.total
+        })
+      
+        const countResult5 = await db.collection('request_info').where({
+          boardingPoint: '知行楼'
+        }).count()
+        this.setData({
+          count5: countResult5.total
+        })
+      
+        const countResult6 = await db.collection('request_info').where({
+          boardingPoint: '求是楼'
+        }).count()
+        this.setData({
+          count6: countResult6.total
+        })
+      
+        const countResult7 = await db.collection('request_info').where({
+          boardingPoint: '南苑'
+        }).count()
+        this.setData({
+          count7: countResult7.total
+        })
+      
+        const countResult8 = await db.collection('request_info').where({
+          boardingPoint: '校医务室'
+        }).count()
+        this.setData({
+          count8: countResult8.total
+        })
+      
+        const countResult9 = await db.collection('request_info').where({
+          boardingPoint: '第二食堂'
+        }).count()
+        this.setData({
+          count9: countResult9.total
+        })
+  }
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- DynamicBus云开发/miniprogram/pages/request/request.js
+++ DynamicBus云开发/miniprogram/pages/request/request.js
@@ -1,105 +1,113 @@
-// pages/request/request.js
+let submitCount = 0;
 Page({
-
-    
-    //页面的初始数据
-    
-    data: {
-        select: false,  //先进行初始化，判断下拉列表是显示隐藏的
-        boardingPoint: '请选择',  //先初始化一个值（内容）
-        SelectTwo: false,  
-        dropoffPoint: '请选择' 
+  // 页面的初始数据
+  data: {
+    select: false,            //下拉列表初始状态是隐藏
+    boardingPoint: '请选择',
+    SelectTwo: false,
+    dropoffPoint: '请选择'
+  },
+ 
+    // 点击事件
+     //取反，类似开关操作，点击显示下拉列表
+     bindShowMsg() {
+        this.setData({
+            select:!this.data.select
+        })
     },
-   
-    
-    //  点击事件
-   //  点击的时候控制下拉列表是显示的
-     //取反，类似开关操作
-    bindShowMsg() {
-      this.setData({
-          select:!this.data.select
-      })
-  },
-    bindShowMsgTwo() {
-      this.setData({
-        SelectTwo:!this.data.SelectTwo
-      })
-  },
-    
-   
-    //  点击的时候接收传过来的值,选中的上下车地点
-    //  赋值到这个变量上boardingPoint
-    //  并且隐藏下拉列表 
-     
-  mySelect(e) {
-     var name = e.currentTarget.dataset.name
-     this.setData({
-        boardingPoint: name,
-         select: false
-     })
-     
-  },
-  mySelectTwo(e) {
-     var name = e.currentTarget.dataset.name
-     this.setData({
-        dropoffPoint: name,
-          SelectTwo: false
-     })
-  },
-  request(){
+      bindShowMsgTwo() {
+        this.setData({
+          SelectTwo:!this.data.SelectTwo
+        })
+    },
+      
+      // 选择上车、下车地点并赋值到这个变量到boardingPoint、dropoffPoint，
+     //  选择结束，隐藏下拉列表
+    mySelect(e) {
+       var name = e.currentTarget.dataset.name
+       this.setData({
+          boardingPoint: name,
+           select: false
+       })
+       
+    },
+    mySelectTwo(e) {
+       var name = e.currentTarget.dataset.name
+       this.setData({
+          dropoffPoint: name,
+            SelectTwo: false
+       })
+    },
+  
+
+  request() {
     // 获取登录状态
     var isLogin = wx.getStorageSync('isLogin');
     if (isLogin) {
-        const that = this
+      const that = this
 
-        //判断上下车地点是否相同
-        if(that.data.boardingPoint == that.data.dropoffPoint){
-            wx.showToast({
-              title: '请重新选择',
-              icon: 'error',
-              duration: 2000
-            })
-            return
-        }
-        
-        //判断上下车地点有一个为空
-        else if(that.data.boardingPoint!=='请选择' && that.data.dropoffPoint!=='请选择'){
-        wx.navigateTo({
-            url: '../Viewing/Viewing?boardingPoint='+that.data.boardingPoint+'&dropoffPoint='+that.data.dropoffPoint,
-            success(res){
+      // 判断上下车地点是否相同
+      if (that.data.boardingPoint == that.data.dropoffPoint) {
+        wx.showToast({
+          title: '请重新选择',
+          icon: 'error',
+          duration: 2000
+        })
+        return
+      }
+
+
+      // 判断上下车地点是否有存在空
+      else if (that.data.boardingPoint !== '请选择' && that.data.dropoffPoint !== '请选择') {
+
+         //5分钟内，点击次数不能超过3次
+        if (submitCount < 3) {
+          submitCount++;
+          setTimeout(() => {
+            submitCount = 0;
+          }, 5 * 60 * 1000); // 5分钟，单位为毫秒
+
+          //跳转到非tabBar页面，并传参
+          wx.navigateTo({
+            url: '../Viewing/Viewing?boardingPoint=' + that.data.boardingPoint + '&dropoffPoint=' + that.data.dropoffPoint,
+            success(res) {
               console.log(res)
-            },fail(err){
+            }, fail(err) {
               console.error(err)
             }
-        })
-        }else{
-            wx.showToast({
-                title: '请重新选择',
-                icon: 'error',
-                duration: 2000
-              })
-              return
+          });
+
+        } else {
+          wx.showToast({
+            title: '提交频繁，请稍后',
+            icon: 'none'
+          });
         }
-       
-        
-
-    } else{
-        // 未登录，弹出提示框并跳转到登录页面
-        wx.showToast({ 
-            title: '您还没有登录', 
-            icon: 'error',
-            duration: 2000 
-            }) 
-        setTimeout(function() {
-            wx.switchTab({
-                url: '../loginin/loginin'
-            })
-        }, 2000);
+      } else {
+        wx.showToast({
+          title: '请重新选择',
+          icon: 'error',
+          duration: 2000
+        })
+        return
+      }
+    } else {
+      // 未登录，弹出提示框并跳转到登录页面
+      wx.showToast({
+        title: '您还没有登录',
+        icon: 'error',
+        duration: 2000
+      })
+      setTimeout(function () {
+        wx.switchTab({
+          url: '../loginin/loginin'
+        })
+      }, 2000);
     }
-
-
-   
   }
+});
 
 
-  })
+
+
+
